abf2eeb37cc780ecbfe8a64852b47c7d5e6343f7
Dustin Rohde <dustin.rohde@gmail.com>
2020-03-27 14:43:57 -0400

    docstrings

--- conway/__init__.py
+++ conway/__init__.py
@@ -18,6 +18,10 @@ def run(
     sep: str = DEFAULT_SEP,
     out: IO = DEFAULT_OUTFILE,
 ):
+    """Run the Game of Life to completion.
+
+    See the ``--help`` output for details.
+    """
     render(grid, sep, out)
     time.sleep(delay)
 
@@ -31,6 +35,13 @@ def run(
 def run_iter(
     grid: BaseGrid, sep: str = DEFAULT_SEP, turns: int = DEFAULT_TURNS
 ) -> Iterator[str]:
+    """Iterate over each tick of the Game.
+
+    Yields a string representation the state of the Game after each tick. The
+    first frame yielded is the initial state of the game.
+
+    See the `run` method for argument details.
+    """
     yield draw(grid, sep)
     while turns:
         grid.tick()
@@ -39,8 +50,10 @@ def run_iter(
 
 
 def render(grid: BaseGrid, sep: str = DEFAULT_SEP, out: IO = DEFAULT_OUTFILE):
+    """Print the `grid` to `out` prefixed with the given `sep`."""
     print(draw(grid, sep), file=out)
 
 
 def draw(grid: BaseGrid, sep: str = DEFAULT_SEP) -> str:
+    """Draw the `grid` prefixed with the given `sep`."""
     return f"{sep}\n{grid}"
