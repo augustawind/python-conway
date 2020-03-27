b61ed45bc29711ec250d207c0f798e37b39efb2e
Dustin Rohde <dustin.rohde@gmail.com>
2020-03-27 19:01:50 -0400

    progress

--- conway_cli/__main__.py
+++ conway_cli/__main__.py
@@ -1,12 +1,18 @@
 import argparse
+import pprint
+import shlex
 import sys
 from argparse import Namespace
 from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
-import prompt_toolkit.shortcuts.prompt
-from prompt_toolkit.completion import FuzzyWordCompleter, PathCompleter
-from prompt_toolkit.validation import ValidationError, Validator
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import (
+    NestedCompleter,
+    PathCompleter,
+    WordCompleter,
+)
+from prompt_toolkit.validation import Validator as PTValidator
 
 SAMPLE_DIR = Path(__file__).parent.parent.absolute() / "sample_patterns"
 SAMPLE_CHOICES = ("beacon", "blinker", "glider", "toad")
@@ -30,10 +36,18 @@ pause
 ValidatorT = Callable[[Any], Any]
 
 
+class ValidationError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+    def __str__(self):
+        return f"error: {self.message}"
+
+
 class CustomArgumentParser(argparse.ArgumentParser):
     def error(self, message: str):
         self.print_usage(sys.stdout)
-        raise ValidationError(message=message)
+        raise ValidationError(message)
 
 
 class Argument:
@@ -43,10 +57,16 @@ class Argument:
         validator: Optional[ValidatorT] = None,
         **kwargs,
     ):
-        self.parser = CustomArgumentParser()
-        action = self.parser.add_argument(*name_or_flags, **kwargs)
-        self.name = action.dest
         self.validator = validator
+        self.parser = CustomArgumentParser(add_help=False)
+        action = self.parser.add_argument(*name_or_flags, **kwargs)
+
+        self.name = action.dest
+        self.completions = {}
+        if action.option_strings:
+            if len(action.option_strings) > 1:
+                self.completions[action.option_strings[-2]] = ""
+            self.completions[action.option_strings[-1]] = action.help or ""
 
     def parse(
         self, argv: List[str], ns: Namespace
@@ -79,6 +99,12 @@ class Command:
             ns, argv = argument.parse(argv, ns)
         return ns
 
+    def get_completions(self) -> dict:
+        completions: dict = {}
+        for argument in self.arguments:
+            completions.update(argument.completions)
+        return completions
+
 
 def define_commands() -> Dict[str, Command]:
     # --------------------------------------------
@@ -86,6 +112,8 @@ def define_commands() -> Dict[str, Command]:
 
     set_grid = Command("set-grid", "Set the Grid.")
     set_grid.add_arguments(
+        Argument("-w", "--width", type=int, help="the width of the grid"),
+        Argument("-h", "--height", type=int, help="the height of the grid"),
         Argument(
             "--random",
             type=float,
@@ -128,7 +156,7 @@ def define_commands() -> Dict[str, Command]:
 
     def validate_gt_zero(n: int):
         if n < 1:
-            raise ValidationError(message="number must be > 0")
+            raise ValidationError("number must be > 0")
         return n
 
     tick.add_arguments(
@@ -141,27 +169,45 @@ def define_commands() -> Dict[str, Command]:
 COMMANDS = define_commands()
 
 
-def prompt(prefix: str = "> ", **prompt_kwargs) -> str:
-    return prompt_toolkit.shortcuts.prompt(
+def is_valid_command(text: str) -> bool:
+    return bool(text) and (text.split(maxsplit=1)[0] in COMMANDS)
+
+
+def start_session(prefix: str = "> ", **session_kwargs) -> PromptSession:
+    completion_dict = {
+        cmd.name: cmd.get_completions() for cmd in COMMANDS.values()
+    }
+
+    return PromptSession(
         prefix,
-        completer=FuzzyWordCompleter(
-            list(COMMANDS),
-            {name: cmd.description for name, cmd in COMMANDS.items()},
+        completer=NestedCompleter(
+            {
+                name: WordCompleter(list(completions), meta_dict=completions)
+                for name, completions in completion_dict.items()
+            }
         ),
         complete_in_thread=True,
         complete_while_typing=True,
-        **prompt_kwargs,
+        validator=PTValidator.from_callable(
+            is_valid_command, error_message="invalid command"
+        ),
+        **session_kwargs,
     )
 
 
 def run_cli():
+    session = start_session()
     while True:
-        cmd = prompt()
+        input_ = session.prompt()
+        cmd_name, *args = shlex.split(input_)
+        print(f"CMD => {cmd_name}")
+        cmd = COMMANDS[cmd_name]
+        ns = cmd.parse(args)
+        pprint.pprint(vars(ns))
 
 
 def main():
-    cmd = prompt()
-    print(f"CMD => {cmd}")
+    run_cli()
 
 
 if __name__ == "__main__":
